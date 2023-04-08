5bacd6695f78ae682eb6cd7109416a0f463562f3
IxPrumxI <walidaqeel@gmail.com>
2023-04-08 07:26:46 +0300

    Show more data

--- src/main/java/me/prum/lorecopier/LoreProcesser.java
+++ src/main/java/me/prum/lorecopier/LoreProcesser.java
@@ -1,11 +1,11 @@
 package me.prum.lorecopier;
 
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import nl.kyllian.enums.Expire;
 import nl.kyllian.models.Paste;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,8 +14,13 @@ import java.util.List;
 public class LoreProcesser {
 
     public static String processOne(ItemStack item) {
-        CompoundTag nbt = item.serializeNBT();
-        return upload(nbt.toString());
+        JSONObject itemObject = new JSONObject();
+
+        itemObject.put("nbt", item.serializeNBT().toString());
+        itemObject.put("hovername", item.getHoverName().getString());
+        itemObject.put("displayname", item.getDisplayName().getString());
+
+        return upload(itemObject.toString());
     }
 
     public static String processInventory(Inventory inventory) {
@@ -25,7 +30,14 @@ public class LoreProcesser {
             if(itemStack.getCount() == 0 || itemStack.isEmpty() || itemStack.getItem().equals(Items.AIR)) {
                 continue;
             }
-            loreArray.add(itemStack.serializeNBT().toString());
+            JSONObject itemObject = new JSONObject();
+
+            itemObject.put("nbt", itemStack.serializeNBT().toString());
+            itemObject.put("hovername", itemStack.getHoverName().getString());
+            itemObject.put("displayname", itemStack.getDisplayName().getString());
+            itemObject.put("slot", i);
+
+            loreArray.add(itemObject.toString());
         }
 
         String lore = "[" + String.join(",", loreArray) + "]";
